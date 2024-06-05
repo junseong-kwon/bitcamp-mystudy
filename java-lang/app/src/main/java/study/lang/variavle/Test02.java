0d93c896eb2db9814bc6f0f1016fe5587c42736b
junseong-kwon <rnjswnstjd13@gmail.com>
2024-06-05 17:14:02 +0900

    .

--- java-lang/app/src/main/java/study/lang/variavle/Test02.java
+++ java-lang/app/src/main/java/study/lang/variavle/Test02.java
@@ -12,12 +12,12 @@ public class Test02 {
     char c1 = 44032; // 변수에 '가' 문자의 코드를 정수 값으로 저장하라
     char c2 = '\uAC00'; // 변수에 '가' 문자의 코드를 형태의 유니코드 표현법으로 저장하라.
     char c3 = '가'; // 변수에 '가 문자의 코드를 가장 쉬운 방법으로 저장하라.
-    char c4 = '\uac00';
+
 
     System.out.println(c1);
     System.out.println(c2);
     System.out.println(c3);
-    System.out.println(c4);
+    System.out.println(0b0010);
 
 
 
