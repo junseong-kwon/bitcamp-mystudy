ac7f35fbd54ceb7a8c22bf9e29ff49e47e2b57a2
junseong-kwon <rnjswnstjd13@gmail.com>
2024-06-11 19:56:51 +0900

    ;

---
+++ inflearn/java study/src/While1_1.java
@@ -0,0 +1,17 @@
+public class While1_1 {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        while(count < 3);
+
+        System.out.println("현재 숫자는:" + count);
+        System.out.println("현재 숫자는:" + count);
+        System.out.println("현재 숫자는:" + count);
+        System.out.println("현재 숫자는:" + count);
+
+
+        }
+    }
+
