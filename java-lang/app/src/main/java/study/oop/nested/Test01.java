94a39d6ca25f9881ef24612c3818676599af6a24
junseong-kwon <rnjswnstjd13@gmail.com>
2024-07-15 23:48:06 +0900

    7월 15일

---
+++ java-lang/app/src/main/java/study/oop/nested/Test01.java
@@ -0,0 +1,17 @@
+package study.oop.nested;
+
+public class Test01 {
+  interface Printer {
+    void print();
+  }
+
+  public static void main(String[] args) {
+
+    Printer obj -> System.out.println("Hello!");
+  }
+}
+
+
+
+
+
