7e01841e076b81231614a9b9e478e039507cb339
junseong-kwon <rnjswnstjd13@gmail.com>
2024-07-23 21:58:04 +0900

    7월 23일

---
+++ myapp/app/src/main/java/javamid2/generic/animal/Cat.java
@@ -0,0 +1,14 @@
+package javamid2.generic.animal;
+
+public class Cat extends Animal{
+
+
+    public Cat( String name, int size) {
+        super(name, size);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("냐옹");
+    }
+}
